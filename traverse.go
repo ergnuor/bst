92071756a3c3bcd9d95ecb4be07b0524bd55a9ef
Denis Tarasov <brokeneye@gmail.com>
2019-10-01 22:52:59 +0300

    Code duplication removal

--- traverse.go
+++ traverse.go
@@ -1,6 +1,16 @@
 package bst
 
-import "math"
+import (
+	"math"
+)
+
+type order byte
+
+const (
+	pre order = iota
+	in
+	post
+)
 
 type Visitor interface {
 	Visit(Payload)
@@ -37,63 +47,15 @@ func (q *queue) Empty() bool {
 }
 
 func TraversePreOrder(t Tree, v Visitor) {
-	n := t.Root()
-	nodeStack := make([]Node, 0, t.MaxHeight())
-
-	for l := len(nodeStack); n != nil || l > 0; l = len(nodeStack) {
-		if n != nil {
-			v.Visit(n.Payload())
-			nodeStack = append(nodeStack, n)
-			n = n.Left()
-			continue
-		}
-
-		n = nodeStack[l-1]
-		n = n.Right()
-		nodeStack = nodeStack[:l-1]
-	}
+	orderTraverse(t, v, pre)
 }
 
 func TraverseInOrder(t Tree, v Visitor) {
-	n := t.Root()
-	nodeStack := make([]Node, 0, t.MaxHeight())
-
-	for l := len(nodeStack); n != nil || l > 0; l = len(nodeStack) {
-		if n != nil {
-			nodeStack = append(nodeStack, n)
-			n = n.Left()
-			continue
-		}
-
-		n = nodeStack[l-1]
-		v.Visit(n.Payload())
-		n = n.Right()
-		nodeStack = nodeStack[:l-1]
-	}
+	orderTraverse(t, v, in)
 }
 
 func TraversePostOrder(t Tree, v Visitor) {
-	n := t.Root()
-	nodeStack := make([]Node, 0, t.MaxHeight())
-	var lastVisitedNode Node
-
-	for l := len(nodeStack); n != nil || l > 0; l = len(nodeStack) {
-		if n != nil {
-			nodeStack = append(nodeStack, n)
-			n = n.Left()
-			continue
-		}
-
-		n = nodeStack[l-1]
-		if n.Right() != nil && n.Right() != lastVisitedNode {
-			n = n.Right()
-		} else {
-			v.Visit(n.Payload())
-			lastVisitedNode = n
-			nodeStack = nodeStack[:l-1]
-			n = nil
-		}
-	}
+	orderTraverse(t, v, post)
 }
 
 func TraverseBreadthFirst(t Tree, v Visitor) {
@@ -112,5 +74,40 @@ func TraverseBreadthFirst(t Tree, v Visitor) {
 		if n.Right() != nil {
 			q.Enqueue(n.Right())
 		}
+	}
+}
+
+func orderTraverse(t Tree, v Visitor, o order) {
+	n := t.Root()
+	nodeStack := make([]Node, 0, t.MaxHeight())
+	var lastPoppedNode Node
+
+	for l := len(nodeStack); n != nil || l > 0; l = len(nodeStack) {
+		if n != nil {
+			if o&pre != 0 {
+				v.Visit(n.Payload())
+			}
+
+			nodeStack = append(nodeStack, n)
+			n = n.Left()
+			continue
+		}
+
+		n = nodeStack[l-1]
+		if o&in != 0 && (n.Right() == nil || n.Right() != lastPoppedNode) {
+			v.Visit(n.Payload())
+		}
+
+		if n.Right() != nil && n.Right() != lastPoppedNode {
+			n = n.Right()
+			continue
+		}
+
+		if o&post != 0 {
+			v.Visit(n.Payload())
+		}
+		lastPoppedNode = n
+		nodeStack = nodeStack[:l-1]
+		n = nil
 	}
 }
